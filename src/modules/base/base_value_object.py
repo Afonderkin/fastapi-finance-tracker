abeaf97f647ec1179757379935bded4ce8131d21
AfonderkinS <afonderkin_stas@mail.ru>
2025-03-05 12:09:19 +0300

    add str method

--- src/modules/base/base_value_object.py
+++ src/modules/base/base_value_object.py
@@ -8,3 +8,6 @@ VT = TypeVar("VT", bound=Any)
 @dataclass(frozen=True)
 class BaseValueObject(Generic[VT]):
     value: VT
+
+    def __str__(self) -> VT:
+        return self.value
--- src/modules/categories/domain/entities/categories.py
+++ src/modules/categories/domain/entities/categories.py
@@ -7,3 +7,6 @@ from modules.categories.domain.value_objects import Title
 @dataclass(frozen=True)
 class Category(Base[int]):
     title: Title
+
+    def __str__(self) -> str:
+        return f"Category(id={self.id}, title={self.title})"
