7ca0e13bdab1f62bb2fa83af8b1debcc03c7fc27
AfonderkinS <afonderkin_stas@mail.ru>
2025-03-06 20:40:16 +0300

    create base_service

--- src/modules/base/__init__.py
+++ src/modules/base/__init__.py
@@ -2,8 +2,10 @@ __all__ = (
     "BaseEntity",
     "BaseModel",
     "BaseValueObject",
+    "BaseService",
 )
 
 from modules.base.base_entity import BaseEntity
 from modules.base.base_model import BaseModel
+from modules.base.base_service import BaseService
 from modules.base.base_value_object import BaseValueObject
--- src/modules/categories/domain/services/category_service.py
+++ src/modules/categories/domain/services/category_service.py
@@ -1,42 +1,22 @@
 from typing import TYPE_CHECKING
 
-from modules.categories.domain.entities import CategoryEntity
+from modules.base import BaseService
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
     from modules.categories.domain.interfaces import ICategoryRepository
 
 
-class CategoryService:
+class CategoryService(BaseService["ICategoryRepository", "CategoryEntity"]):
     def __init__(
         self,
-        session: AsyncSession,
-        repo: ICategoryRepository,
+        session: "AsyncSession",
+        repo: "ICategoryRepository",
     ) -> None:
-        self.session = session
-        self.repo = repo
+        super(CategoryService, self).__init__(session, repo)
 
-    async def get_all_categories(self) -> list[CategoryEntity]:
-        categories = [category.to_entity() for category in (await self.repo.find_all())]
-        return categories
-
-    async def get_category_by_id(self, category_id: int) -> CategoryEntity | None:
-        category = await self.repo.find_by_id(category_id)
-        if category:
-            return category.to_entity()
-        return None
-
-    async def get_category_by_title(self, title: str) -> CategoryEntity | None:
+    async def get_by_title(self, title: str) -> "CategoryEntity | None":
         category = await self.repo.find_by_title(title)
         if category:
             return category.to_entity()
         return None
-
-    async def create_category(self, title: str) -> None:
-        await self.repo.save(title=title)
-
-    async def update_category_title(self, category_id: int, new_title: str) -> None:
-        await self.repo.update(category_id=category_id, new_title=new_title)
-
-    async def delete_category(self, category_id: int) -> None:
-        await self.repo.delete(category_id=category_id)
